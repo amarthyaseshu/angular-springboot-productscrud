9acf69ba4dee48db712a11db4b6004b72b3ba7b2
Amarthya <amarthyaseshu2@gmail.com>
2020-09-22 23:39:22 +0530

    FSJ0320B006

--- src/app/addproduct/addproduct.component.ts
+++ src/app/addproduct/addproduct.component.ts
@@ -2,31 +2,32 @@ import { Books } from './../books';
 import { NgserviceService } from './../ngservice.service';
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
-import{NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.component.html',
-  styleUrls: ['./addproduct.component.css']
+  styleUrls: ['./addproduct.component.css'],
 })
 export class AddproductComponent implements OnInit {
-  book=new Books();
+  book = new Books();
 
-  constructor(private _route:Router,private _service:NgserviceService) { }
+  options: any = ['one', 'two', 'three'];
+  constructor(private _route: Router, private _service: NgserviceService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectCat(tempCat: any): any {
+    this.book.bookCategory = tempCat;
   }
-
-  addproductformsubmit(){
-this._service.addProductToRemote(this.book).subscribe(
-  data=>{
-    console.log("data added succesfully");
-    this._route.navigate(['productlist'])
-  },
- error=> console.error("error")
-  
-)
+  addproductformsubmit() {
+    this._service.addProductToRemote(this.book).subscribe(
+      (data) => {
+        console.log('data added succesfully');
+        this._route.navigate(['productlist']);
+      },
+      (error) => console.error('error')
+    );
   }
-
 }
